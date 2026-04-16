16b72272dee9e78035cb843f88a4bcfab508e359
Otávio Miranda <otaviomirandabr19@gmail.com>
2026-04-16 16:12:15 -0300

    deploy: publish updated hostinger landing\n\nCo-Authored-By: Paperclip <noreply@paperclip.ing>

---
+++ site/_next/static/chunks/app/layout-f032c2a4dde59f19.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7108:()=>{},9467:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7108,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[897,587,315,358],()=>s(9467)),_N_E=e.O()}]);
